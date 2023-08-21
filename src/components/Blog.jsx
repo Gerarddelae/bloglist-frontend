96397cbe6a6437f51f95f2a76770debef33a005e
Gerardo <gerardmoe@hotmail.com>
2023-08-21 17:46:03 -0500

    5.7 bloglist

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  
+  const showWhenVisible = { 
+    display: visible ? '' : 'none',
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+  <>
+  <div style={blogStyle}>
+      {blog.title} <button onClick={toggleVisibility}>view</button>
+  </div>
+  <div style={showWhenVisible}>
+    <p>{blog.url}</p>
+    <p>{blog.likes}</p>
+    <p>{blog.author}</p>
+  </div>
+  </>  
+  )
+}
 
 export default Blog
