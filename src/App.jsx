1ed086cbb0eef681bfe1c9ccf52110a690aff776
Gerardo <gerardmoe@hotmail.com>
2023-08-21 18:53:47 -0500

    5.9 bloglist

--- src/App.jsx
+++ src/App.jsx
@@ -138,7 +138,7 @@ const App = () => {
         setNewUrl={setNewUrl}
         />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
       </>
